786bede0ee60568cf1de04cf9026840c9110359e
Brick <51419000+nick-brick@users.noreply.github.com>
2023-10-11 18:58:25 +0300

    First commit

---
+++ docs/js/script.js
@@ -0,0 +1,50 @@
+const btnDarkMode=document.querySelector(".dark-mode-btn");
+
+
+
+// 1.Проверка темной темы на уровне системных настроек
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
+    btnDarkMode.classList.add('dark-mode-btn--active');
+    document.body.classList.add("dark");
+}
+
+// 2.Проверка темной темы в local storage
+if (localStorage.getItem('darkMode')==='dark'){
+    btnDarkMode.classList.add('dark-mode-btn--active');
+    document.body.classList.add("dark");
+} else if (localStorage.getItem('darkMode')==='light'){
+    btnDarkMode.classList.remove('dark-mode-btn--active');
+    document.body.classList.remove("dark");
+}
+
+// 3.Изменение ситемных настроек - изменение темы
+window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', (event) => {
+
+    const newColorScheme = event.matches ? "dark" : "light";
+
+    if ( newColorScheme === 'dark'){
+        btnDarkMode.classList.add('dark-mode-btn--active');
+        document.body.classList.add("dark");
+        localStorage.setItem('darkMode', 'dark')
+    } else {
+        btnDarkMode.classList.remove('dark-mode-btn--active');
+        document.body.classList.remove("dark");
+        localStorage.setItem('darkMode', 'light')
+    }
+})
+
+
+
+/* Включение ночного режима по кнопке*/ 
+btnDarkMode.onclick = function(){
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+   const isDark = document.body.classList.toggle("dark");
+
+   if (isDark){
+    localStorage.setItem('darkMode', 'dark')
+   } else {
+    localStorage.setItem('darkMode', 'light')
+   }
+} 
